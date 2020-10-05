d427435fe311a1c8ced0583a27015b8989dd7620
ShunShunNeverGiveUp <285147009@qq.com>
2020-10-05 16:17:48 +0800

    hw3

---
+++ hw3/solar-system/Assets/Scripts/revolution.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class revolution : MonoBehaviour
+{
+    public Transform center;
+    private float speed;
+    private Vector3 normal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = Random.Range(10, 30);
+        normal.y = Random.Range(-50, 0);
+        normal.z = Random.Range(-50, 50);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(center.position, normal, speed * Time.deltaTime);
+    }
+}
---
+++ hw3/solar-system/Assets/Scripts/rotation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.WSA;
+
+public class rotation : MonoBehaviour
+{
+    private float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = Random.Range(30, 60);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(transform.position, Vector3.up, speed * Time.deltaTime);
+    }
+}
