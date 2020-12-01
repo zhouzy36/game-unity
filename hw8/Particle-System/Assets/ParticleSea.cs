009fc403b162dbd03aaf980f52f3d6268bdb7d5b
ShunShunNeverGiveUp <285147009@qq.com>
2020-12-01 20:39:45 +0800

    hw8

---
+++ hw8/Particle-System/Assets/ParticleSea.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParticleSea : MonoBehaviour
+{
+
+    public ParticleSystem particleSystem;
+    private ParticleSystem.Particle[] particlesArray;
+    public int seaResolution = 50;
+    public float spacing = 0.5f;
+    public float noiseScale = 0.2f;
+    public float heightScale = 3f;
+    public Gradient colorGradient;
+
+    private float perlinNoiseAnimX = 0.01f;
+    private float perlinNoiseAnimY = 0.01f;
+    
+
+    void Start()
+    {
+        particleSystem = gameObject.GetComponent<ParticleSystem>();
+        particlesArray = new ParticleSystem.Particle[seaResolution * seaResolution];
+        particleSystem.maxParticles = seaResolution * seaResolution;
+        particleSystem.Emit(seaResolution * seaResolution); //立即发射seaResolution * seaResolution个粒子
+        particleSystem.GetParticles(particlesArray); //获取particlesArray中的粒子
+    }
+
+    private void Update()
+    {
+        for (int i = 0; i < seaResolution; i++)
+        {
+            for (int j = 0; j < seaResolution; j++)
+            {
+                float zPos = Mathf.PerlinNoise(i * noiseScale + perlinNoiseAnimX, j * noiseScale + perlinNoiseAnimY);
+                particlesArray[i * seaResolution + j].color = colorGradient.Evaluate(zPos);
+                particlesArray[i * seaResolution + j].position = new Vector3(i * spacing, zPos * heightScale, j * spacing);
+                //particlesArray[i * seaResolution + j].position = new Vector3(i * spacing, zPos, j * spacing);
+            }
+        }
+
+        perlinNoiseAnimX += 0.01f;
+        perlinNoiseAnimY += 0.01f;
+
+        particleSystem.SetParticles(particlesArray, particlesArray.Length);
+    }
+
+}
