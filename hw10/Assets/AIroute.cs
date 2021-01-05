a67d8a3670864c7f4b88637df40221e5b6e3cf42
ShunShunNeverGiveUp <285147009@qq.com>
2021-01-05 16:41:43 +0800

    hw10

---
+++ hw10/Assets/AIroute.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIroute : MonoBehaviour
+{
+    public GameObject target;  //获取目标点
+    NavMeshAgent agent;   //声明变量
+
+    void Start()
+    {
+        //获取自身的NavMeshAgent组件
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //设置目标
+        agent.SetDestination(target.transform.position);
+    }
+}
---
+++ hw10/Assets/Check.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Check : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "PlayerMainBody")
+        {
+            //Debug.Log("enter");
+            GameObject obj = this.gameObject;
+            obj.GetComponent<NavMeshAgent>().enabled = true;
+            obj.GetComponent<AIroute>().enabled = true;
+            
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.name == "PlayerMainBody")
+        {
+            //Debug.Log("exit");
+            GameObject obj = this.gameObject;
+            obj.GetComponent<NavMeshAgent>().enabled = false;
+            obj.GetComponent<AIroute>().enabled = false;
+        }
+    }
+}
